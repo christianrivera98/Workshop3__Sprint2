5030b8b6ddc85c6f8d1c3438d5fbf4742f9c6a13
Christian <christiancamiloriveralamadrid@gmail.com>
2023-07-27 09:58:15 -0500

    update:new releases/addin pages/components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import './index.scss'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from './pages/Homepage'
+import Planets from './pages/planets'
+import Mission from './pages/mission'
+import Characters from './pages/Characters'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />}  />
+        <Route path='/planets' element={ <Planets />} />
+        <Route path='/missions' element={ <Mission />} />
+        <Route path='/characters' element={ <Characters />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Planets.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Planets = () => {
+  return (
+    <div>planets</div>
+  )
+}
+
+export default Planets
